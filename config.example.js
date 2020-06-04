ce4d815870d655f02430ba54e093c54c73f6aae5
-view <b.buhles@gmail.com>
2020-06-04 17:09:06 +0200

    fuck dependencies

---
+++ config.example.js
@@ -0,0 +1,25 @@
+const config = {
+    
+    //interval in hours
+    "interval": 1,
+
+    //msg delete threshold global
+    // => this will be the time after which
+    //feedback messages by the bot will be deleted
+  
+    "msgdelete": "30000",
+
+
+
+    "ownerID": "686669011601326281",
+  
+    // Bot Admins, Array of user ID strings.
+    /* 
+    !format: ["123456789", "987654321"]
+    */
+    "admins": [],
+  
+    "support": [],
+}
+
+module.exports = config;
--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,5 @@
+const handle = require('../modules/handle')
+
 module.exports = async (client) => {
     
   
@@ -10,5 +12,32 @@ module.exports = async (client) => {
       status: 'online'
     });
     client.logger.ready(`${client.user.username} is ready to serve ${client.users.size} users in ${client.guilds.size} servers.`)
+
+    var CronJob = require('cron').CronJob;
+    var job = new CronJob(
+      `0 * */${client.config.interval} * * *`,
+      async function () {
+        let users = client.users
+        for (let user in users) {
+          if (users.hasOwnProperty(user)) {
+            let data = users[user]
+            let checkvar = handle.get("user", user.id)
+            if (!checkvar) return
+            let h = checkvar.hours
+            if (checkvar.channel) {
+              let hint = parseInt(h)
+              let newt = hint + client.config.interval
+              handle.update("hours", newt, user.id)
+            }
+          }
+        } 
+      },
+      null,
+      true,
+      "America/Los_Angeles"
+    );
+    job.start();
+
+    client.logger.read(`Cron Job started successfully!`)
 
 };
